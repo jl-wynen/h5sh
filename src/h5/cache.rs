a55b16d1364bb369de4786fd0597472520022e45
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-06-27 16:49:30 +0200

    Add insert_child

--- src/h5/cache.rs
+++ src/h5/cache.rs
@@ -1,4 +1,7 @@
-use indexmap::IndexMap;
+use indexmap::{
+    IndexMap,
+    map::Entry::{Occupied, Vacant},
+};
 use smallvec::SmallVec;
 
 use super::path::H5Path;
@@ -21,8 +24,16 @@ pub struct Entry<Value> {
 }
 
 impl<Value> FileCache<Value> {
+    pub fn contains_key(&self, key: &H5Path) -> bool {
+        self.objects.contains_key(key)
+    }
+
     pub fn get<Key: CacheKey<Entry<Value>>>(&self, key: Key) -> Option<&Entry<Value>> {
         key.get_cache_entry(&self.objects)
+    }
+
+    pub fn get_mut<Key: CacheKey<Entry<Value>>>(&mut self, key: Key) -> Option<&mut Entry<Value>> {
+        key.get_cache_entry_mut(&mut self.objects)
     }
 
     pub fn get_with_id(&self, key: &H5Path) -> Option<(EntryId, &Entry<Value>)> {
@@ -31,8 +42,38 @@ impl<Value> FileCache<Value> {
             .map(|(index, _, entry)| (index.into(), entry))
     }
 
+    pub fn get_key(&self, id: EntryId) -> Option<&H5Path> {
+        self.objects.get_index(id.0).map(|(key, _)| key)
+    }
+
     pub fn insert(&mut self, path: H5Path, data: Value) -> EntryId {
         self.objects.insert_full(path, Entry::from(data)).0.into()
+    }
+
+    pub fn insert_child<Key: CacheKey<Entry<Value>>>(
+        &mut self,
+        parent: Key,
+        path: H5Path,
+        data: Value,
+    ) -> Option<EntryId> {
+        // This implementation ensures that the child is only inserted if the parent
+        // exists. It uses unsafe code with pointers to bypass the borrow checker which
+        // would otherwise not allow getting the parent first and then inserting the
+        // child. Alternatively, we could check whether the parent exists first,
+        // insert the child, and then get_mut the parent. But that would require
+        // extra hash operations.
+        self.objects.reserve(1);
+        let parent = self.get_mut(parent)? as *mut Entry<Value>;
+        let child_id = self.insert(path, data);
+        // Safety: The call to reserve above ensures that there is enough space in the
+        // map for the child, so the parent is not moved and the pointer stays valid.
+        unsafe {
+            (*parent)
+                .children
+                .get_or_insert_with(SmallVec::new)
+                .push(child_id);
+        }
+        Some(child_id)
     }
 }
 
@@ -71,11 +112,22 @@ impl<Value> From<Value> for Entry<Value> {
 
 pub trait CacheKey<Entry> {
     fn get_cache_entry<'m>(&self, objects: &'m IndexMap<H5Path, Entry>) -> Option<&'m Entry>;
+    fn get_cache_entry_mut<'m>(
+        &self,
+        objects: &'m mut IndexMap<H5Path, Entry>,
+    ) -> Option<&'m mut Entry>;
 }
 
 impl<Entry> CacheKey<Entry> for H5Path {
     fn get_cache_entry<'m>(&self, objects: &'m IndexMap<H5Path, Entry>) -> Option<&'m Entry> {
         objects.get(self)
+    }
+
+    fn get_cache_entry_mut<'m>(
+        &self,
+        objects: &'m mut IndexMap<H5Path, Entry>,
+    ) -> Option<&'m mut Entry> {
+        objects.get_mut(self)
     }
 }
 
@@ -83,11 +135,25 @@ impl<Entry> CacheKey<Entry> for &H5Path {
     fn get_cache_entry<'m>(&self, objects: &'m IndexMap<H5Path, Entry>) -> Option<&'m Entry> {
         objects.get(*self)
     }
+
+    fn get_cache_entry_mut<'m>(
+        &self,
+        objects: &'m mut IndexMap<H5Path, Entry>,
+    ) -> Option<&'m mut Entry> {
+        objects.get_mut(*self)
+    }
 }
 
 impl<Entry> CacheKey<Entry> for EntryId {
     fn get_cache_entry<'m>(&self, objects: &'m IndexMap<H5Path, Entry>) -> Option<&'m Entry> {
         objects.get_index(self.0).map(|(_, entry)| entry)
+    }
+
+    fn get_cache_entry_mut<'m>(
+        &self,
+        objects: &'m mut IndexMap<H5Path, Entry>,
+    ) -> Option<&'m mut Entry> {
+        objects.get_index_mut(self.0).map(|(_, entry)| entry)
     }
 }
 
@@ -110,6 +176,7 @@ impl CacheValue {
 mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
+    use smallvec::smallvec;
 
     #[test]
     fn get_entry_by_path() {
@@ -157,5 +224,23 @@ mod tests {
         assert_eq!(cache.get(root).unwrap().children(), None);
         assert_eq!(cache.get(a).unwrap().children(), None);
         assert_eq!(cache.get(b).unwrap().children(), None);
+    }
+
+    #[test]
+    fn insert_children() {
+        let root = H5Path::from("/root".to_string());
+        let a = H5Path::from("/root/a".to_string());
+        let b = H5Path::from("/root/b".to_string());
+        let cache = {
+            let mut cache = FileCache::<i32>::default();
+            let root_id = cache.insert(root.clone(), 4);
+            cache.insert_child(root_id, a.clone(), 6);
+            cache.insert_child(root.clone(), b.clone(), 9);
+            cache
+        };
+        let a_id = cache.get_with_id(&a).unwrap().0;
+        let b_id = cache.get_with_id(&b).unwrap().0;
+        let root_entry = cache.get(root).unwrap();
+        assert_eq!(root_entry.children, Some(smallvec![a_id, b_id]));
     }
 }
