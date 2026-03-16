jl-wynen/h5sh

commit 4ded5c8c51f7bdfb54370093037ec956c4ee2506

Collapse if statements
New rust features, pointed out by clippy.

src/data.rs
@@ -178,12 +178,12 @@ mod load_and_format {
         if write!(&mut out, "{}", content.array()).is_err() {
             let _ = write!(&mut out, "<failed write>");
         };
-        if matches!(content, PartialData::Full(_)) {
-            if let Some(max_width) = max_width {
-                if max_width < out.len() {
-                    out.truncate(max_width.saturating_sub(4));
-                    out.push_str(&trailing_ellipses);
-                }
+        if matches!(content, PartialData::Full(_))
+            && let Some(max_width) = max_width
+        {
+            if max_width < out.len() {
+                out.truncate(max_width.saturating_sub(4));
+                out.push_str(&trailing_ellipses);
             }
         } else {
             out.truncate(out.len().saturating_sub(1)); // remove final ']'

src/commands/find.rs
@@ -137,18 +137,16 @@ fn find_name_in_group(
                 printer,
             )?;
         };
-        if recursive {
-            if let H5Object::Group(child_group) = child {
-                let child_path = child_group.path().clone();
-                find_name_in_group(
-                    child_group,
-                    child_path,
-                    absolute_target.join(&path),
-                    pattern,
-                    recursive,
-                    printer,
-                )?;
-            }
+        if recursive && let H5Object::Group(child_group) = child {
+            let child_path = child_group.path().clone();
+            find_name_in_group(
+                child_group,
+                child_path,
+                absolute_target.join(&path),
+                pattern,
+                recursive,
+                printer,
+            )?;
         }
     }
     stdout.flush()?;

src/line_editor/completion/simple_completer.rs
@@ -13,10 +13,10 @@ where
     LoadChildren: Fn(&Value) -> h5::Result<Children>,
     Children: IntoIterator<Item = (H5Path, Value, bool)>,
 {
-    if let Some(entry) = cache.get(current) {
-        if entry.is_leaf() || !current.as_raw().ends_with('/') {
-            return vec![finalize_entry_path(current.as_raw(), entry)];
-        }
+    if let Some(entry) = cache.get(current)
+        && (entry.is_leaf() || !current.as_raw().ends_with('/'))
+    {
+        return vec![finalize_entry_path(current.as_raw(), entry)];
     }
 
     let parent = current.parent();

commit b03e6bdea57f664699b42d5da02d5163519246d9

Fix long text truncation
I broke this in 4ded5c8

src/data.rs
@@ -178,10 +178,10 @@ mod load_and_format {
         if write!(&mut out, "{}", content.array()).is_err() {
             let _ = write!(&mut out, "<failed write>");
         };
-        if matches!(content, PartialData::Full(_))
-            && let Some(max_width) = max_width
-        {
-            if max_width < out.len() {
+        if matches!(content, PartialData::Full(_)) {
+            if let Some(max_width) = max_width
+                && max_width < out.len()
+            {
                 out.truncate(max_width.saturating_sub(4));
                 out.push_str(&trailing_ellipses);
             }