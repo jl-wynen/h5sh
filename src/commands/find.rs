a727692484d88ae8e0ccf003784ac6fe46b4ba43
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2026-03-16 11:41:19 +0100

    Simplify write_matched_path
    This no longer allows for the match to span across directory names. The match can only be in the final part of the path. This is how we apply the pattern already anyway.

--- src/commands/find.rs
+++ src/commands/find.rs
@@ -150,55 +150,48 @@ fn write_matched_path<'q, Q: QueueableCommand>(
     location_type: hdf5::LocationType,
     printer: &Printer,
 ) -> std::io::Result<&'q mut Q> {
-    let mut is_group_style = false;
     if !target.is_current() {
         queue.queue(&printer.style().group)?.queue(Print(target))?;
         if !target.as_raw().ends_with('/') {
             queue.queue(Print('/'))?;
         }
-        is_group_style = true;
+
+        if location_type == hdf5::LocationType::Dataset {
+            // Switch to the dataset style
+            queue
+                .queue(ResetColor)?
+                .queue(SetAttribute(Attribute::Reset))?
+                .queue(&printer.style().dataset)?;
+        } // else: stick with the group style
+    } else {
+        match location_type {
+            hdf5::LocationType::Dataset => {
+                queue.queue(&printer.style().dataset)?;
+            }
+            hdf5::LocationType::Group => {
+                queue.queue(&printer.style().group)?;
+            }
+            _ => {
+                queue.queue(SetAttribute(Attribute::Reset))?;
+            }
+        }
     }
 
-    let name = path.name();
-    let raw = path.as_raw();
-    let path_includes_parent = raw.len() != name.len();
+    queue_with_highlight_range(queue, path.as_raw(), mat.range())?;
 
-    if (path_includes_parent || location_type == hdf5::LocationType::Group) && !is_group_style {
-        queue.queue(&printer.style().group)?;
-    }
-
-    let color_change = if path_includes_parent {
-        raw.len() - name.len()
-    } else if location_type == hdf5::LocationType::Group {
-        raw.len()
-    } else {
-        0
-    };
-
-    if color_change <= mat.start() {
-        queue
-            .queue(Print(&raw[..color_change]))?
-            .queue(ResetColor)?
-            .queue(SetAttribute(Attribute::Reset))?;
-        queue_with_highlight_range(queue, &raw[color_change..], mat.range())?;
-    } else {
-        queue_with_highlight_range(queue, &raw[..color_change], mat.range())?;
-        queue
-            .queue(ResetColor)?
-            .queue(SetAttribute(Attribute::Reset))?
-            .queue(Print(&raw[color_change..]))?;
-    }
-
-    let character = if location_type == hdf5::LocationType::Group {
-        GROUP_CHARACTER
-    } else {
-        DATASET_CHARACTER
+    queue
+        .queue(ResetColor)?
+        .queue(SetAttribute(Attribute::Reset))?;
+    let character = match location_type {
+        hdf5::LocationType::Dataset => DATASET_CHARACTER,
+        hdf5::LocationType::Group => GROUP_CHARACTER,
+        _ => None,
     };
     if let Some(character) = character {
         queue.queue(Print(character))?;
     }
 
-    queue.queue(Print('\n'))
+    queue.queue(Print("\n"))
 }
 
 fn find_attr(
@@ -406,128 +399,280 @@ impl FromStr for Pattern {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    mod parser {
+        use super::super::*;
 
-    fn parse_args(args: &[&str]) -> Arguments {
-        Arguments::from_arg_matches(
-            &Find
-                .arg_parser()
-                .no_binary_name(true)
-                .try_get_matches_from(args)
-                .unwrap(),
-        )
-        .unwrap()
-    }
+        fn parse_args(args: &[&str]) -> Arguments {
+            Arguments::from_arg_matches(
+                &Find
+                    .arg_parser()
+                    .no_binary_name(true)
+                    .try_get_matches_from(args)
+                    .unwrap(),
+            )
+            .unwrap()
+        }
 
-    fn assert_pattern_name(pattern: &Pattern, expected: &str) {
-        match pattern {
-            Pattern::Name(name) => {
-                assert_eq!(name.as_str(), expected);
+        fn assert_pattern_name(pattern: &Pattern, expected: &str) {
+            match pattern {
+                Pattern::Name(name) => {
+                    assert_eq!(name.as_str(), expected);
+                }
+                _ => {
+                    panic!("expected a Name pattern, got Attr")
+                }
             }
-            _ => {
-                panic!("expected a Name pattern, got Attr")
+        }
+
+        fn assert_pattern_attr_key(pattern: &Pattern, expected: &str) {
+            match pattern {
+                Pattern::Attr { name, value } => {
+                    assert_eq!(name.as_str(), expected);
+                    assert!(value.is_none());
+                }
+                _ => {
+                    panic!("expected an Attr pattern, got Name")
+                }
             }
+        }
+
+        fn assert_pattern_attr_key_value(
+            pattern: &Pattern,
+            expected_name: &str,
+            expected_value: &str,
+        ) {
+            match pattern {
+                Pattern::Attr { name, value } => {
+                    assert_eq!(name.as_str(), expected_name);
+                    assert_eq!(value.as_ref().unwrap().as_str(), expected_value);
+                }
+                _ => {
+                    panic!("expected an Attr pattern, got Name")
+                }
+            }
+        }
+
+        #[test]
+        fn parse_pattern_name() {
+            let args = parse_args(&["some_name"]);
+            assert_pattern_name(&args.pattern, "some_name");
+            assert_eq!(args.target, H5Path::from("."));
+        }
+
+        #[test]
+        fn parse_pattern_name_with_target() {
+            let args = parse_args(&["needle", "farm/barn"]);
+            assert_pattern_name(&args.pattern, "needle");
+            assert_eq!(args.target, H5Path::from("farm/barn"));
+        }
+
+        #[test]
+        fn parse_pattern_name_with_equal() {
+            let args = parse_args(&["a=b"]);
+            assert_pattern_name(&args.pattern, "a=b");
+            assert_eq!(args.target, H5Path::from("."));
+        }
+
+        #[test]
+        fn parse_pattern_name_with_trailing_equal() {
+            let args = parse_args(&["name="]);
+            assert_pattern_name(&args.pattern, "name=");
+            assert_eq!(args.target, H5Path::from("."));
+        }
+
+        #[test]
+        fn parse_pattern_attribute_name() {
+            let args = parse_args(&["@key"]);
+            assert_pattern_attr_key(&args.pattern, "key");
+            assert_eq!(args.target, H5Path::from("."));
+        }
+
+        #[test]
+        fn parse_pattern_attribute_with_value() {
+            let args = parse_args(&["@abc=2hs"]);
+            assert_pattern_attr_key_value(&args.pattern, "abc", "2hs");
+            assert_eq!(args.target, H5Path::from("."));
+        }
+
+        #[test]
+        fn parse_pattern_attribute_with_value_extra_equal() {
+            let args = parse_args(&["@asd=qwe=zxc"]);
+            assert_pattern_attr_key_value(&args.pattern, "asd", "qwe=zxc");
+            assert_eq!(args.target, H5Path::from("."));
+        }
+
+        #[test]
+        fn parse_pattern_attribute_with_value_trailing_equal() {
+            let args = parse_args(&["@asd=qwe="]);
+            assert_pattern_attr_key_value(&args.pattern, "asd", "qwe=");
+            assert_eq!(args.target, H5Path::from("."));
+        }
+
+        #[test]
+        fn parse_pattern_attribute_with_empty_value() {
+            let args = parse_args(&["@abc="]);
+            assert_pattern_attr_key_value(&args.pattern, "abc", "");
+            assert_eq!(args.target, H5Path::from("."));
+        }
+
+        #[test]
+        fn parse_pattern_attribute_without_key() {
+            let args = parse_args(&["@=value"]);
+            assert_pattern_attr_key_value(&args.pattern, "", "value");
+            assert_eq!(args.target, H5Path::from("."));
+        }
+
+        #[test]
+        fn parse_pattern_attribute_with_value_with_target() {
+            let args = parse_args(&["@2_3=iU", "/entry/path/"]);
+            assert_pattern_attr_key_value(&args.pattern, "2_3", "iU");
+            assert_eq!(args.target, H5Path::from("/entry/path/"));
         }
     }
 
-    fn assert_pattern_attr_key(pattern: &Pattern, expected: &str) {
-        match pattern {
-            Pattern::Attr { name, value } => {
-                assert_eq!(name.as_str(), expected);
-                assert!(value.is_none());
-            }
-            _ => {
-                panic!("expected an Attr pattern, got Name")
-            }
+    mod printer {
+        use super::super::*;
+        use crossterm::execute;
+
+        fn write_match(
+            target: &H5Path,
+            path: &H5Path,
+            pattern: &str,
+            location_type: hdf5::LocationType,
+        ) -> String {
+            let mat = Regex::new(pattern).unwrap().find(path.as_raw()).unwrap();
+
+            let printer = Printer::new();
+            let mut buffer: Vec<u8> = Vec::new();
+            write_matched_path(&mut buffer, target, path, mat, location_type, &printer).unwrap();
+            String::from_utf8(buffer).unwrap()
         }
-    }
 
-    fn assert_pattern_attr_key_value(pattern: &Pattern, expected_name: &str, expected_value: &str) {
-        match pattern {
-            Pattern::Attr { name, value } => {
-                assert_eq!(name.as_str(), expected_name);
-                assert_eq!(value.as_ref().unwrap().as_str(), expected_value);
-            }
-            _ => {
-                panic!("expected an Attr pattern, got Name")
-            }
+        #[test]
+        fn write_match_dataset_in_cwd() {
+            let target = H5Path::from(".");
+            let path = H5Path::from("foo");
+            let res = write_match(&target, &path, "oo", hdf5::LocationType::Dataset);
+
+            let mut buffer: Vec<u8> = Vec::new();
+            execute!(
+                buffer,
+                Print("f"),
+                SetAttribute(Attribute::Underlined),
+                Print("oo"),
+                SetAttribute(Attribute::NoUnderline),
+                ResetColor,
+                SetAttribute(Attribute::Reset),
+                Print("\n")
+            )
+            .unwrap();
+            let expected = String::from_utf8(buffer).unwrap();
+
+            assert_eq!(res, expected);
         }
-    }
 
-    #[test]
-    fn parse_pattern_name() {
-        let args = parse_args(&["some_name"]);
-        assert_pattern_name(&args.pattern, "some_name");
-        assert_eq!(args.target, H5Path::from("."));
-    }
+        #[test]
+        fn write_match_group_in_cwd() {
+            let target = H5Path::from(".");
+            let path = H5Path::from("group");
+            let res = write_match(&target, &path, "rou", hdf5::LocationType::Group);
 
-    #[test]
-    fn parse_pattern_name_with_target() {
-        let args = parse_args(&["needle", "farm/barn"]);
-        assert_pattern_name(&args.pattern, "needle");
-        assert_eq!(args.target, H5Path::from("farm/barn"));
-    }
+            let mut buffer: Vec<u8> = Vec::new();
+            execute!(
+                buffer,
+                &Printer::new().style().group,
+                Print("g"),
+                SetAttribute(Attribute::Underlined),
+                Print("rou"),
+                SetAttribute(Attribute::NoUnderline),
+                Print("p"),
+                // Some extra reset in case the dataset style is different from the default:
+                ResetColor,
+                SetAttribute(Attribute::Reset),
+                Print("/\n")
+            )
+            .unwrap();
+            let expected = String::from_utf8(buffer).unwrap();
 
-    #[test]
-    fn parse_pattern_name_with_equal() {
-        let args = parse_args(&["a=b"]);
-        assert_pattern_name(&args.pattern, "a=b");
-        assert_eq!(args.target, H5Path::from("."));
-    }
+            assert_eq!(res, expected);
+        }
 
-    #[test]
-    fn parse_pattern_name_with_trailing_equal() {
-        let args = parse_args(&["name="]);
-        assert_pattern_name(&args.pattern, "name=");
-        assert_eq!(args.target, H5Path::from("."));
-    }
+        #[test]
+        fn write_match_dataset_in_subdir_full_match() {
+            let target = H5Path::from("folder");
+            let path = H5Path::from("foo");
+            let res = write_match(&target, &path, "foo", hdf5::LocationType::Dataset);
 
-    #[test]
-    fn parse_pattern_attribute_name() {
-        let args = parse_args(&["@key"]);
-        assert_pattern_attr_key(&args.pattern, "key");
-        assert_eq!(args.target, H5Path::from("."));
-    }
+            let mut buffer: Vec<u8> = Vec::new();
+            execute!(
+                buffer,
+                &Printer::new().style().group,
+                Print("folder/"),
+                ResetColor,
+                SetAttribute(Attribute::Reset),
+                SetAttribute(Attribute::Underlined),
+                Print("foo"),
+                SetAttribute(Attribute::NoUnderline),
+                ResetColor,
+                SetAttribute(Attribute::Reset),
+                Print("\n")
+            )
+            .unwrap();
+            let expected = String::from_utf8(buffer).unwrap();
 
-    #[test]
-    fn parse_pattern_attribute_with_value() {
-        let args = parse_args(&["@abc=2hs"]);
-        assert_pattern_attr_key_value(&args.pattern, "abc", "2hs");
-        assert_eq!(args.target, H5Path::from("."));
-    }
+            assert_eq!(res, expected);
+        }
 
-    #[test]
-    fn parse_pattern_attribute_with_value_extra_equal() {
-        let args = parse_args(&["@asd=qwe=zxc"]);
-        assert_pattern_attr_key_value(&args.pattern, "asd", "qwe=zxc");
-        assert_eq!(args.target, H5Path::from("."));
-    }
+        #[test]
+        fn write_match_dataset_in_subdir_match_end() {
+            let target = H5Path::from("folder");
+            let path = H5Path::from("foo");
+            let res = write_match(&target, &path, "oo", hdf5::LocationType::Dataset);
 
-    #[test]
-    fn parse_pattern_attribute_with_value_trailing_equal() {
-        let args = parse_args(&["@asd=qwe="]);
-        assert_pattern_attr_key_value(&args.pattern, "asd", "qwe=");
-        assert_eq!(args.target, H5Path::from("."));
-    }
+            let mut buffer: Vec<u8> = Vec::new();
+            execute!(
+                buffer,
+                &Printer::new().style().group,
+                Print("folder/"),
+                ResetColor,
+                SetAttribute(Attribute::Reset),
+                Print("f"),
+                SetAttribute(Attribute::Underlined),
+                Print("oo"),
+                SetAttribute(Attribute::NoUnderline),
+                ResetColor,
+                SetAttribute(Attribute::Reset),
+                Print("\n")
+            )
+            .unwrap();
+            let expected = String::from_utf8(buffer).unwrap();
 
-    #[test]
-    fn parse_pattern_attribute_with_empty_value() {
-        let args = parse_args(&["@abc="]);
-        assert_pattern_attr_key_value(&args.pattern, "abc", "");
-        assert_eq!(args.target, H5Path::from("."));
-    }
+            assert_eq!(res, expected);
+        }
 
-    #[test]
-    fn parse_pattern_attribute_without_key() {
-        let args = parse_args(&["@=value"]);
-        assert_pattern_attr_key_value(&args.pattern, "", "value");
-        assert_eq!(args.target, H5Path::from("."));
-    }
+        #[test]
+        fn write_match_group_in_subdir() {
+            let target = H5Path::from("folder");
+            let path = H5Path::from("group");
+            let res = write_match(&target, &path, "rou", hdf5::LocationType::Group);
 
-    #[test]
-    fn parse_pattern_attribute_with_value_with_target() {
-        let args = parse_args(&["@2_3=iU", "/entry/path/"]);
-        assert_pattern_attr_key_value(&args.pattern, "2_3", "iU");
-        assert_eq!(args.target, H5Path::from("/entry/path/"));
+            let mut buffer: Vec<u8> = Vec::new();
+            execute!(
+                buffer,
+                &Printer::new().style().group,
+                Print("folder/g"),
+                SetAttribute(Attribute::Underlined),
+                Print("rou"),
+                SetAttribute(Attribute::NoUnderline),
+                Print("p"),
+                ResetColor,
+                SetAttribute(Attribute::Reset),
+                Print("/\n")
+            )
+            .unwrap();
+            let expected = String::from_utf8(buffer).unwrap();
+
+            assert_eq!(res, expected);
+        }
     }
 }
